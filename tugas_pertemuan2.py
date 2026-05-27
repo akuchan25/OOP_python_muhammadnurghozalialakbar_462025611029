61d9b791b9f4c2554138abfb9dc4d843d46cbf2f
akuchan25 <ghozali.akbar99@gmail.com>
2026-05-27 21:35:32 +0700

    Add files via upload

---
+++ tugas_pertemuan2.py
@@ -0,0 +1,33 @@
+class kantor:
+    pass
+class pegawai:
+    pass
+
+kantor1 = kantor()
+kantor2 = kantor()
+kantor3 = kantor()
+
+print(kantor1)
+print(kantor2)
+print(kantor3)
+
+pegawai1 = pegawai()
+pegawai1.nama = "azu"
+pegawai1.nmr = "01"
+
+pegawai2 = pegawai()
+pegawai2.nama = "momi"
+pegawai2.nmr = "02"
+
+pegawai3 = pegawai()
+pegawai3.nama = "mako"
+pegawai3.nmr = "03"
+
+print(pegawai1.nama)
+print(pegawai1.nmr)
+
+print(pegawai2.nama)
+print(pegawai2.nmr)
+
+print(pegawai3.nama)
+print(pegawai3.nmr)
